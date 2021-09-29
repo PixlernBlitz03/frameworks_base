ezio84/abc-n_frameworks_base

Ambient music: pulse on new music tracks and Now Playing info [1/2]

core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -53,7 +53,8 @@ public class AmbientDisplayConfiguration {
                 || pickupGestureEnabled(user)
                 || tapGestureEnabled(user)
                 || doubleTapGestureEnabled(user)
-                || isPowerBtnFlashlightEnabled(user);
+                || isPowerBtnFlashlightEnabled(user)
+                || isAmbientTickerEnabled(user);
     }
 
     /** {@hide} */
@@ -224,5 +225,11 @@ public class AmbientDisplayConfiguration {
     public boolean isPowerBtnFlashlightEnabled(int user) {
         return Settings.Secure.getIntForUser(mContext.getContentResolver(),
                 Settings.Secure.TORCH_POWER_BUTTON_GESTURE, 0, user) != 0;
+    }
+
+    /** {@hide} */
+    public boolean isAmbientTickerEnabled(int user) {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.PULSE_ON_NEW_TRACKS, 1, user) != 0;
     }
 }

PixlernBlitz03/frameworks_base

core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -52,7 +52,8 @@ public class AmbientDisplayConfiguration {
                 || wakeDisplayGestureEnabled(user)
                 || pickupGestureEnabled(user)
                 || tapGestureEnabled(user)
-                || doubleTapGestureEnabled(user);
+                || doubleTapGestureEnabled(user)
+                || isAmbientTickerEnabled(user);
     }
 
     /** {@hide} */
@@ -217,5 +218,10 @@ public class AmbientDisplayConfiguration {
 
     private boolean boolSetting(String name, int user, int def) {
         return Settings.Secure.getIntForUser(mContext.getContentResolver(), name, def, user) != 0;
+    }
+    /** {@hide} */
+    public boolean isAmbientTickerEnabled(int user) {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.PULSE_ON_NEW_TRACKS, 1, user) != 0;
     }
 }